2a43b0f8ccc95fa9d9437684243d6834f655979b
flo100500 <florian@team-fink.de>
2024-11-16 00:33:59 +0100

    5

--- logic/strategy.py
+++ logic/strategy.py
@@ -2,15 +2,13 @@ from typing import List
 from models.game_state import GameState
 from models.player_action import PlayerAction
 from models.base import Base
-from models.game_state import GameState
+from models.position import Position
 import math
-
-TEAM_ID : int = 7
 
 def is_our_base_id(gameState : GameState, id : int) -> bool:
     for base in gameState.bases:
         if base.uid == id:
-            return base.player == TEAM_ID
+            return base.player == gameState.game.player
     return False
         
 def get_base_from_id(gameState : GameState, id) -> Base:
@@ -32,7 +30,7 @@ def get_nearest_enemy_base(gameState : GameState, distances_to_bases : dict[int,
     current_min = 100000
     for base_id, distance in distances_to_bases.items():
         base = get_base_from_id(gameState, base_id)
-        if distance < current_min and distance != 0 and base.player != TEAM_ID:
+        if distance < current_min and distance != 0 and base.player != gameState.game.player:
             base_id_of_current_min = base_id
             current_min = distance
     return base_id_of_current_min
@@ -55,40 +53,77 @@ def help_bits_needed(gamestate: GameState, attack_on_bases_dict:dict, baseuid: i
             amount += values[0]
     return amount
 
+def find_middle(bases : list[Base]) -> Position:
+    x, y, z = 0, 0, 0
+    for base in bases:
+        x += base.position.x
+        y += base.position.y
+        z += base.position.z
+    n_bases = len(bases)
+    x /= n_bases
+    y /= n_bases
+    z /= n_bases
+    return Position(x, y, z)
+
+def find_rand_direction(gamestate: GameState, all_bases, our_bases) -> Position:
+    middle = find_middle(all_bases)
+    our_middle = find_middle(our_bases)
+
+    x = our_middle.x - middle.x
+    y = our_middle.y - middle.y
+    z = our_middle.z - middle.z
+
+    return Position(x, y, z)
+
+
+def calc_angle(a : Position, b : Position) -> float:
+    return math.acos((a.x * b.x + a.y * b.y + a.z * b.z) / (math.sqrt(a.x**2 + a.y**2 + a.z**2) * math.sqrt(b.x**2 + b.y**2 + b.z**2)))
+
 # def get_list_of_bases_how_need_help(gameState : GameState) -> list[int]:
 #     for base in gameState.bases:
 #         if base.player != 7:
 #             continue
         
 
-def decide(gameState: GameState) -> List[PlayerAction]:
+def decide(gamestate: GameState) -> List[PlayerAction]:
     playeractions_list = []
 
-    attack_on_bases = check_for_enemy_attack(gameState)
+    all_bases = gamestate.bases
+    our_bases : list[Base] = []
+    for base in all_bases:
+        if is_our_base_id(gamestate, base.id):
+            our_bases.append(base)
 
-    for base in gameState.bases:
-        if base.player != TEAM_ID:
-            continue
-        distances_to_bases = calc_distances_to_bases(gameState, base)
-        nearest_enemy_base_id = get_nearest_enemy_base(gameState, distances_to_bases)
-        distance = calc_distance(base, get_base_from_id(gameState, nearest_enemy_base_id))
-        grace = gameState.config.paths.grace_period
-        death_rate = gameState.config.paths.death_rate
-        deat_players = (distance - grace) * death_rate if grace < distance else 0
+    our_middle = find_middle(our_bases)
 
-        max_population = gameState.config.base_levels[base.level].max_population
-        population_0_75 = int(0.75 * max_population)
+    attack_on_bases = check_for_enemy_attack(gamestate)
+    rand_direction = find_rand_direction(gamestate, all_bases, our_bases)
 
-        if base.population > population_0_75 and help_bits_needed(gameState, attack_on_bases, base.uid) <= 10 and not deat_players:
-            playeractions_list.append(PlayerAction(base.uid, nearest_enemy_base_id, base.population - population_0_75))
-        elif  base.population >= max_population:
-            if deat_players and not len(gameState.config.base_levels) - 1 == base.level:
-                playeractions_list.append(PlayerAction(base.uid, base.uid, base.population - max_population + gameState.config.base_levels[base.level].spawn_rate))
-            else:
-                amount = base.population - max_population + gameState.config.base_levels[base.level].spawn_rate
-                if amount > death_rate:
-                    playeractions_list.append(PlayerAction(base.uid, nearest_enemy_base_id, amount))
-                else:
-                    playeractions_list.append(PlayerAction(base.uid, nearest_enemy_base_id, death_rate))
+    targets = []
+    i : int = 0
+    while not len(targets):
+        i += 1
+        for our_base in our_bases:
+            distances_to_bases : dict[int,int] = calc_distances_to_bases(gamestate, our_base)
+            for base_id, distance in distances_to_bases:
+                if distance == i and base_id not in targets and not is_our_base_id(base_id):
+                    targets.append(base_id)
+
+    min_score_base = None
+    for target_id in targets:
+        min_score = 100000
+        min_score_base = None
+        target : Base = get_base_from_id(target_id)
+        x = target.x - our_middle.x
+        y = target.y - our_middle.y
+        z = target.z - our_middle.z
+        score = target.population + 3 * calc_angle(rand_direction, Position(x,y,z))
+        if min_score > score:
+            min_score = score
+            min_score_base = target
+
+    for our_base in our_bases:
+        if our_base.population > 15:
+            playeractions_list.append(PlayerAction(our_base.uid, min_score_base.uid, 1))
 
     return playeractions_list
