84fa687517190a3da042a191602beb8c0e578ade
flo100500 <florian@team-fink.de>
2024-11-16 03:06:09 +0100

    9

--- logic/strategy.py
+++ logic/strategy.py
@@ -137,7 +137,7 @@ def decide(gameState: GameState) -> List[PlayerAction]:
             wanted_player_num = gameState.config.base_levels[our_base.level].max_population/2
             if our_base.population > wanted_player_num  and calc_distance(our_base, min_score_base) <= gameState.config.paths.grace_period:
                 playeractions_list.append(PlayerAction(our_base.uid, min_score_base.uid, our_base.population - wanted_player_num))
-            elif our_base.population > 15:
+            elif our_base.population > wanted_player_num:
                 playeractions_list.append(PlayerAction(our_base.uid, our_base.uid, our_base.population - wanted_player_num))
 
     return playeractions_list
